5d16715967568c09a6cc3e67dc2b3616e1aff9b1
pradiptadewan <pradiptadwn13@gmail.com>
2025-07-06 22:45:41 +0700

    next

--- rekomendasi_buku/settings.py
+++ rekomendasi_buku/settings.py
@@ -20,17 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7b#uxin7@4kzcpe+cnjlw+s&4e9s5gstw-6!1)siod#9%hytoe'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-
-ALLOWED_HOSTS = []
+SECRET_KEY = os.environ.get('SECRET_KEY', 'secret-local')
+DEBUG = os.environ.get('DEBUG', '') == '1'
+ALLOWED_HOSTS = ['*'] 
 
 
 # Application definition
@@ -43,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sistem'
+    'whitenoise.runserver_nostatic'
 ]
 
 MIDDLEWARE = [
@@ -53,6 +46,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'rekomendasi_buku.urls'
@@ -83,11 +77,19 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': 'rekomendasi-buku',  # Nama database Anda
-        'USER': 'postgres',           # Username database Anda
+        'USER': 'postgres',           # Username database Anda  
         'PASSWORD': 'dewan123',   # Password database Anda
         'HOST': 'localhost',
         'PORT': '5432',
     }
+}
+
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 
@@ -125,8 +127,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+import os
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
